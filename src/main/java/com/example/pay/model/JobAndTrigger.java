25c770e4ff9df520da3e2d7e948279b5e3d69ede
magesxl <lbchocolate@126.com>
2018-04-07 13:43:56 +0800

    修改jackson转化报错问题   实现序列化

--- src/main/java/com/example/pay/model/JobAndTrigger.java
+++ src/main/java/com/example/pay/model/JobAndTrigger.java
@@ -1,13 +1,13 @@
 package com.example.pay.model;
 
-import lombok.Data;
+
+import java.io.Serializable;
 
 /**
  * 作业详情类
  */
 
-@Data
-public class JobAndTrigger {
+public class JobAndTrigger implements Serializable{
 
     private String  jobName;
 
@@ -27,5 +27,75 @@ public class JobAndTrigger {
 
     private String  descrption;
 
+    public String getJobName() {
+        return jobName;
+    }
 
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getJobGroup() {
+        return jobGroup;
+    }
+
+    public void setJobGroup(String jobGroup) {
+        this.jobGroup = jobGroup;
+    }
+
+    public String getJobClassName() {
+        return jobClassName;
+    }
+
+    public void setJobClassName(String jobClassName) {
+        this.jobClassName = jobClassName;
+    }
+
+    public String getTriggerName() {
+        return triggerName;
+    }
+
+    public void setTriggerName(String triggerName) {
+        this.triggerName = triggerName;
+    }
+
+    public String getTriggerGroup() {
+        return triggerGroup;
+    }
+
+    public void setTriggerGroup(String triggerGroup) {
+        this.triggerGroup = triggerGroup;
+    }
+
+    public String getCronExpression() {
+        return cronExpression;
+    }
+
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    public String getTriggerState() {
+        return triggerState;
+    }
+
+    public void setTriggerState(String triggerState) {
+        this.triggerState = triggerState;
+    }
+
+    public String getTimeZoneId() {
+        return timeZoneId;
+    }
+
+    public void setTimeZoneId(String timeZoneId) {
+        this.timeZoneId = timeZoneId;
+    }
+
+    public String getDescrption() {
+        return descrption;
+    }
+
+    public void setDescrption(String descrption) {
+        this.descrption = descrption;
+    }
 }
