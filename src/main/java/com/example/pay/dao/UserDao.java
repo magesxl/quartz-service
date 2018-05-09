f620c1372c14cadcfee6a5ab63e18b1c19d8e857
dongjianwei1 <dongjianwei1@mail.open.com,cn>
2018-05-09 17:36:34 +0800

    提交批量修改

--- src/main/java/com/example/pay/dao/UserDao.java
+++ src/main/java/com/example/pay/dao/UserDao.java
@@ -3,10 +3,16 @@ package com.example.pay.dao;
 
 
 import com.example.pay.model.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserDao {
     List<User> queryUser();
     int update(User user);
+
+    void batchUpdateOrderByMerId(@Param("list") List<User> users);
+
+    int batchUpdateByPaDefenceId(Map<String,Object> params);
 }
