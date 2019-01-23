cfc2fc09aab844e564431712abe0eed4967c888a
dongjianwei1 <dongjianwei1@mail.open.com,cn>
2019-01-23 14:31:55 +0800

    卡验证工具类

---
+++ src/main/java/com/example/pay/util/BankCardBitValidation.java
@@ -0,0 +1,58 @@
+package com.example.pay.util;
+
+/**
+ * 校验银行卡号是否准确
+ */
+public class BankCardBitValidation {
+
+
+    /*
+    校验过程：
+    1、从卡号最后一位数字开始，逆向将奇数位(1、3、5等等)相加。
+    2、从卡号最后一位数字开始，逆向将偶数位数字，先乘以2（如果乘积为两位数，将个位十位数字相加，即将其减去9），再求和。
+    3、将奇数位总和加上偶数位总和，结果应该可以被10整除。
+    */
+    public static boolean checkBankCard(String bankCard) {
+        if (bankCard.length() < 15 || bankCard.length() > 19) {
+            return false;
+        }
+        char bit = getBankCardCheckCode(bankCard.substring(0, bankCard.length() - 1));
+        if (bit == 'N') {
+            return false;
+        }
+        return bankCard.charAt(bankCard.length() - 1) == bit;
+    }
+
+
+    /**
+     * 从不含校验位的银行卡卡号采用 Luhm 校验算法获得校验位
+     *
+     * @param nonCheckCodeCardId
+     * @return
+     */
+    public static char getBankCardCheckCode(String nonCheckCodeCardId) {
+        if (nonCheckCodeCardId == null || nonCheckCodeCardId.trim().length() == 0
+                || !nonCheckCodeCardId.matches("\\d+")) {
+            //如果传的不是数据返回N
+            return 'N';
+        }
+        char[] chs = nonCheckCodeCardId.trim().toCharArray();
+        int luhmSum = 0;
+        for (int i = chs.length - 1, j = 0; i >= 0; i--, j++) {
+            int k = Character.getNumericValue(chs[i]);//chs[i] - '0';
+            if (j % 2 == 0) {
+                k *= 2;
+                k = k / 10 + k % 10;
+            }
+            luhmSum += k;
+        }
+        return (luhmSum % 10 == 0) ? '0' : (char) ((10 - luhmSum % 10) + '0');  //这边+'0'，不是拼接，在Java和C#中是8+0的ASCII码得到8在ASCII中的编码值，然后通过(char)转成字符'8'
+    }
+
+    public static void main(String[] args) {
+        String cardId = "621797452000327208";
+        boolean result = checkBankCard(cardId);
+        System.out.println(result);
+        System.out.println((int) 11 / 10);
+    }
+}
